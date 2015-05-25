096409495d3da2e3fb3e4db701a94099579e4d10
Forrest Erickson <forresterickson@comcast.net>
2015-05-25 14:52:50 +0000

    Add gaurage door buttons. Call new Python scrip to which pin number can be passed.

--- homecontrol.php
+++ homecontrol.php
@@ -7,16 +7,27 @@
 <body>
 <h4>Home Control:</h4>
 <h5>RPi Camera, Garage Doors, Lights</h5>
+
+<form method="get" action="./homecontrol.php">
+
+<b>IO Port (LED) Control:</b>
+<!--<input style = "color:red;border: 3pt ridge lightgrey; background-color:#000011" type="submit" value="LED ON" name="on">
+--!>
+<input style = "color:red; border: 2pt ridge black" type="submit" value="LED ON" name="on">
+<input style = "color:green; border: 2pt ridge black" type="submit" value="LED OFF" name="off"><br><br>
+<input style = "color:blue; border: 2pt ridge black" type="submit" value="Gaurage West" name="pulse12"><br><br>
+<input style = "color:yellow; border: 2pt ridge black" type="submit" value="Gaurage East" name="pulse18"><br><br>
+</form>
+
 <!--Web page table of buttons-->
 <?
-$ROWS = 3;
+$ROWS = 4;
 $COLUMNS = 5;
 $row = 0;
 $column = 0;
 
-echo '<form method="get" action="./homecontrol.php">';
-
 //Make a table COLUMNS by ROWS
+echo '<form method="get" action="./homecontrol.php">';
 echo '<table border = 1px solid black>';
 for ($row = 0; $row<$ROWS; $row++)
 {
@@ -40,16 +51,6 @@ echo '</table>';
 echo '</form>';
 ?>
 
-<form method="get" action="./homecontrol.php">
-
-<b>IO Port (LED) Control:</b>
-<!--<input style = "color:red;border: 3pt ridge lightgrey; background-color:#000011" type="submit" value="LED ON" name="on">
---!>
-<input style = "color:red; border: 2pt ridge black" type="submit" value="LED ON" name="on">
-<input style = "color:green; border: 2pt ridge black" type="submit" value="LED OFF" name="off"><br><br>
-</form>
-
-
 <?php
 //echo shell_exec("python -V 2>&1");	//Used to test python exicution.
 //echo "PHP text here.<br>";
@@ -70,7 +71,15 @@ else if(isset($_GET['off'])){
 	echo "LED is off";
 }
 
+else if(isset($_GET['pulse12'])){
+	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 12 2>&1");
+	echo "Pulsing low pin 12";
+}
 
+else if(isset($_GET['pulse18'])){
+	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 18 2>&1");
+	echo "Pulsing low pin 18";
+}
 
 
 ?>
