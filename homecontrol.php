755ae9cc9d84408726a5e28bd24bfe19e1d8d951
Forrest Erickson <forresterickson@comcast.net>
2015-05-25 15:08:52 +0000

    Move table of buttons down and tidy up the garauge door buttons.

--- homecontrol.php
+++ homecontrol.php
@@ -10,15 +10,48 @@
 
 <form method="get" action="./homecontrol.php">
 
-<b>IO Port (LED) Control:</b>
 <!--<input style = "color:red;border: 3pt ridge lightgrey; background-color:#000011" type="submit" value="LED ON" name="on">
 --!>
 <input style = "color:red; border: 2pt ridge black" type="submit" value="LED ON" name="on">
 <input style = "color:green; border: 2pt ridge black" type="submit" value="LED OFF" name="off"><br><br>
-<input style = "color:blue; border: 2pt ridge black" type="submit" value="Gaurage West" name="pulse12"><br><br>
+<input style = "color:blue; border: 2pt ridge black" type="submit" value="Gaurage West" name="pulse12">
 <input style = "color:yellow; border: 2pt ridge black" type="submit" value="Gaurage East" name="pulse18"><br><br>
 </form>
 
+<?php
+//Proccess arguments
+//echo shell_exec("python -V 2>&1");	//Used to test python exicution.
+//echo "PHP text here.<br>";
+
+if(isset($_GET['on'])){
+//	$gpio_on = shell_exec("/usr/local/bin/gpio -g write 17 1");
+//	$gpio_off = shell_exec("sudo python /home/pi/Documents/Python/LED_off_Pin7.py");
+	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/LED_on_Pin7.py 2>&1");
+	echo $gpio_on;
+	echo "LED is on";
+}
+
+
+
+else if(isset($_GET['off'])){
+//	$gpio_off = shell_exec("/usr/local/bin/gpio -g write 17 0");
+	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/LED_off_Pin7.py 2>&1");
+	echo "LED is off";
+}
+
+else if(isset($_GET['pulse12'])){
+	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 12 2>&1");
+	echo "Pulsing low pin 12";
+}
+
+else if(isset($_GET['pulse18'])){
+	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 18 2>&1");
+	echo "Pulsing low pin 18";
+}
+
+
+?>
+<hr>
 <!--Web page table of buttons-->
 <?
 $ROWS = 4;
@@ -51,38 +84,6 @@ echo '</table>';
 echo '</form>';
 ?>
 
-<?php
-//echo shell_exec("python -V 2>&1");	//Used to test python exicution.
-//echo "PHP text here.<br>";
-
-if(isset($_GET['on'])){
-//	$gpio_on = shell_exec("/usr/local/bin/gpio -g write 17 1");
-//	$gpio_off = shell_exec("sudo python /home/pi/Documents/Python/LED_off_Pin7.py");
-	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/LED_on_Pin7.py 2>&1");
-	echo $gpio_on;
-	echo "LED is on";
-}
-
-
-
-else if(isset($_GET['off'])){
-//	$gpio_off = shell_exec("/usr/local/bin/gpio -g write 17 0");
-	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/LED_off_Pin7.py 2>&1");
-	echo "LED is off";
-}
-
-else if(isset($_GET['pulse12'])){
-	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 12 2>&1");
-	echo "Pulsing low pin 12";
-}
-
-else if(isset($_GET['pulse18'])){
-	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 18 2>&1");
-	echo "Pulsing low pin 18";
-}
-
-
-?>
 <hr>
 <?
 date_default_timezone_set("America/New_York");
