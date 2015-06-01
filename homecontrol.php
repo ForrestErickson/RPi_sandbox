0385140442d36e5d1f6a519289fcec11076f472b
Forrest Erickson <forresterickson@comcast.net>
2015-06-01 23:03:18 +0000

    Change method of LED and Garage doors from GET to POST.

--- homecontrol.php
+++ homecontrol.php
@@ -1,3 +1,6 @@
+<!-- Code to interface to garage door opener open button and the safty sensor -->
+<!-- F. Lee Erickson, 1 June 2015 -->
+
 <html>
 <head>
 <meta name="viewport" content="width=device-width" />
@@ -8,18 +11,18 @@
 <h4>Home Control:</h4>
 <h5>RPi Camera, Garage Doors, Lights</h5>
 
-<form method="get" action="./homecontrol.php">
-
+<form method="post" action="./homecontrol.php">
+<h6>Solderless Breadboard LED</h6>
 <!--<input style = "color:red;border: 3pt ridge lightgrey; background-color:#000011" type="submit" value="LED ON" name="on">
 --!>
 <input style = "color:red; border: 2pt ridge black" type="submit" value="LED ON" name="on">
 <input style = "color:green; border: 2pt ridge black" type="submit" value="LED OFF" name="off"><br><br>
+
 <input style = "color:blue; border: 2pt ridge black" type="submit" value="Garage West" name="pulse18">
-<input style = "color:magenta; border: 2pt ridge black" type="submit" value="Garage East" name="pulse12"><br><br>
-<input style = "color:cyan; border: 2pt ridge black" type="submit" value="Input 11" name="input11"><br>
+<input style = "color:magenta; border: 2pt ridge black" type="submit" value="Garage East" name="pulse12"><br>
 
 <?
-if(isset($_GET['input11'])){
+if(isset($_POST['input11'])){
 	$gpio = trim(shell_exec("sudo python /home/pi/Documents/Python/Check_Pin.py 11 2>&1"));
 //	echo "gpio is =", $gpio, ".<br>";
 	if (strcasecmp ($gpio, "LOW") ==0 ) {
@@ -30,7 +33,8 @@ if(isset($_GET['input11'])){
 	}
 }
 ?>
-
+<br><br>
+<input style = "color:cyan; border: 2pt ridge black" type="submit" value="Input 11" name="input11">
 </form>
 
 <?php
@@ -38,13 +42,14 @@ if(isset($_GET['input11'])){
 //echo shell_exec("python -V 2>&1");	//Used to test python exicution.
 //echo "PHP text here.<br>";
 
-if(isset($_GET['input11'])){
+//Act on the form buttons.
+if(isset($_POST['input11'])){
 	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Check_Pin.py 11 2>&1");
 	echo "Pin 11 is ";
 	echo ($gpio_on) ;
 }
 
-if(isset($_GET['on'])){
+if(isset($_POST['on'])){
 	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/LED_on_Pin7.py 2>&1");
 	echo $gpio_on;
 	echo "LED is on";
@@ -52,17 +57,17 @@ if(isset($_GET['on'])){
 
 
 
-else if(isset($_GET['off'])){
+else if(isset($_POST['off'])){
 	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/LED_off_Pin7.py 2>&1");
 	echo "LED is off";
 }
 
-else if(isset($_GET['pulse12'])){
+else if(isset($_POST['pulse12'])){
 	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 12 2>&1");
 	echo "Pulsing low pin 12";
 }
 
-else if(isset($_GET['pulse18'])){
+else if(isset($_POST['pulse18'])){
 	$gpio_on = shell_exec("sudo python /home/pi/Documents/Python/Pulse_Low_Pin.py 18 2>&1");
 	echo "Pulsing low pin 18";
 }
